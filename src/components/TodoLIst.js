bfce77b59557c98d685c3abdda781d28411326bf
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-07 17:15:03 -0500

    Define needed states and functionalities

--- src/components/TodoLIst.js
+++ src/components/TodoLIst.js
@@ -1,6 +1,24 @@
 import React from "react";
+import { useState } from 'react';
 
 const TodoList = () => {
+	const [todos, setTodos] = useState([])
+	const [newTodo, setNewTodo] = useState("")
+
+	const handleAddTodo = () => {
+		setTodos([...todos, { text: newTodo, checked: false }])
+		setNewTodo("")
+	}
+
+	const handleDeleteTodo = (index) => {
+
+	}
+
+	const handleToggleTodo = (index) => {
+
+	}
+
+
 	return (
 		<>
 			<h1>To-Do List</h1>
