7ce2ed5996a9a2f42fda33be880930afb5ca767d
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-05-07 17:23:10 -0500

    Create JSX elements

--- src/components/TodoLIst.js
+++ src/components/TodoLIst.js
@@ -1,29 +1,43 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 
 const TodoList = () => {
-	const [todos, setTodos] = useState([])
-	const [newTodo, setNewTodo] = useState("")
+	const [todos, setTodos] = useState([]);
+	const [newTodo, setNewTodo] = useState("");
 
 	const handleAddTodo = () => {
-		setTodos([...todos, { text: newTodo, checked: false }])
-		setNewTodo("")
-	}
+		setTodos([...todos, { text: newTodo, checked: false }]);
+		setNewTodo("");
+	};
 
-	const handleDeleteTodo = (index) => {
+	const handleDeleteTodo = (index) => { };
 
-	}
-
-	const handleToggleTodo = (index) => {
-
-	}
-
+	const handleToggleTodo = (index) => { };
 
 	return (
 		<>
 			<h1>To-Do List</h1>
+			<input
+				type="text"
+				value={newTodo}
+				onChange={(e) => setNewTodo(e.target.value)}
+			/>
+			<button onClick={handleAddTodo}>Add</button>
+			<ul>
+				{todos.map((todo, index) => (
+					<li key={index}>
+						<input
+							type="checkbox"
+							checked={todo.checked}
+							onChange={() => handleToggleTodo(index)}
+						/>
+						<span>{todo.text}</span>
+						<button onClick={() => handleDeleteTodo(index)}>Delete</button>
+					</li>
+				))}
+			</ul>
 		</>
-	)
-}
+	);
+};
 
-export default TodoList
+export default TodoList;
